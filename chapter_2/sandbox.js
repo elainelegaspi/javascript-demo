ef139206bd7180eb27bbd465a690c2c7357da18d
elainelegaspi <143445067+elainelegaspi@users.noreply.github.com>
2025-05-08 11:07:41 -0500

    Chapter 2
    Sandbox Strings Data Types

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,53 @@
+console.log(1);
+console.log(2);
+
+let myAge = 25;
+let year = 2019;
+
+// log things to console
+console.log(myAge, year);
+
+age = 30;
+console.log(age);
+
+const points = 100; // use const if you don't want it to be overriden
+console.log(points);
+
+var score = 75;
+console.log(score);
+
+// strings
+console.log('hello, world');
+
+let email = 'email@academic.rrc.ca';
+console.log(email);
+
+// string concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
+// common string methods
+// let result2 = email.lastIndexOf('a');
+// let result3 = email.slice(0,5);
+// console.log(result2);
+// console.log(result3);
+// let result = email.substr(4,10);
+// let result = email.replace('m', 'w');
+// console.log(result);
