a89d28c7e07cce870fbd5c05f215eca13f8195bd
elainelegaspi <143445067+elainelegaspi@users.noreply.github.com>
2025-05-12 08:39:48 -0500

    Loops and Conditions
    Added conditions and updated loops

---
+++ chapter_3/conditions.js
@@ -0,0 +1,74 @@
+// IF STATEMENTS
+
+// const age = 20;
+
+// if(age > 20){
+//     console.log('You are over 20 years old')
+// }
+
+// const ninjas = ['sean', 'kratos', 'atreus', 'yoshi'];
+
+// if(ninjas.length > 3){
+//     console.log("That's a lot of ninjas")
+// }
+
+// ELSE IF 
+
+// const password = 'p@ssword1234';
+
+// if(password.length >= 12){
+//     console.log('that password is mighty strong');
+// } else if(password.length >= 8){
+//     console.log('that password is long enough!');
+// } else {
+//     console.log('password is not long enough');
+// }
+
+// LOGICAL OPERATORS - OR || and AND &&
+
+// const password = 'p@ss';
+
+// if(password.length >= 12 && password.includes('@')){
+//     console.log('that password is mighty strong');
+// } else if(password.length >= 8 || password.includes('@') && password.length >= 5){
+//     console.log('that password is strong enough!');
+// } else {
+//     console.log('password is not strong enough');
+// }
+
+// logical NOT (!)
+
+// let user = false;
+
+// if(!user){
+//     console.log('you must be loggined in to continue')
+// }
+
+// console.log(!true);
+// console.log(!false);
+
+// switch statements
+
+// using if statements
+
+// const grade = 'D';
+
+// switch(grade){
+//     case 'A':
+//         console.log('You got an A!');
+//         break;
+//     case 'B':
+//         console.log('You got an B!');
+//         break;
+//     case 'C':
+//         console.log('You got an C!');
+//         break;
+//     case 'D':
+//         console.log('You got an D!');
+//         break;
+//     case 'E':
+//         console.log('You got an E!');
+//         break;
+//     default:
+//         console.log('not a valid grade');
+// }
