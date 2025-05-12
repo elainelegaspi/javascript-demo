38dd15e62460ad05d0f1b745f8bf095233d36207
elainelegaspi <143445067+elainelegaspi@users.noreply.github.com>
2025-05-12 10:20:50 -0500

    Added Functions
    Added Function types

---
+++ chapter_4/functions.js
@@ -0,0 +1,59 @@
+// function declaration
+// function greet(){
+//     console.log('hello there');
+// }
+
+// function expression
+// const speak = function(){
+//     console.log('good day');
+// };
+
+
+// arguments & parameters
+// const speak = function(name, time){
+//     console.log(`good ${time} ${name}`);
+// };
+
+
+// greet();
+// greet();
+
+// speak('mario', 'morning');
+
+// Returning values
+
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// }
+
+// const area = calcArea(5);
+// console.log(area);
+
+// ARROW FUNCTIONS
+// const calcArea = radius => 3.14 * radius**2;
+
+// const area = calcArea(5);
+// console.log('area is', area);
+
+// const greet = () => 'hello world';
+// const result = greet();
+// console.log(result);
+
+// const bill = (products, tax) => {
+//     let total = 0
+//     for (let i = 0; i < products.length; i++){}
+// }
+
+const name = 'shaun';
+
+// functions
+const greet = () => 'hello';
+
+let resultOne =  greet();
+console.log(resultOne);
+
+// methods
+
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
+
