63b21937874b5586d87052feb625a3f5adfdd0af
elainelegaspi <143445067+elainelegaspi@users.noreply.github.com>
2025-05-13 09:11:30 -0500

    Object Models
    Added Object Models

---
+++ chapter_5/objects.js
@@ -0,0 +1,46 @@
+// OBJECT LITERALS  
+
+// const blogs = [
+//     { title: 'why mac & cheese rules', likes: 30 },
+//     { title: '10 things to make with marmite', likes: 50 }
+// ];
+
+// let user = {
+//     name: 'crystal',
+//     age: 30,
+//     email: 'crystal@thenetninja.co.uk',
+//     location: 'berlin',
+//     blogs: [  { title: 'why mac & cheese rules', likes: 30 },
+//     { title: '10 things to make with marmite', likes: 50 }],
+//     login(){
+//         console.log('the user logged in');
+//     },
+//     logout()
+//     {
+//         console.log('the user logged out');
+//     },
+//     logBlogs(){
+//         console.log('this user has written the following blogs:');
+//         this.blogs.forEach(blog => {console.log(blog.title, blog.likes);})
+//     }
+// };
+
+// user.login();
+// user.logout();
+// user.logBlogs();
+// console.log(this);
+
+// const name = 'mario';
+// name.toUpperCase();
+
+// console.log(user);
+// console.log(user.name);
+
+// user.age = 35;
+// console.log(user.age);
+
+// console.log(user['location']);
+// user['name'] = 'chun-li';
+// console.log(user['name']);
+
+// console.log(typeof user);
